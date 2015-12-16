866948aa810631a7c2fa95b0ca1afcdc13416512
ww <ww.cs.sjtu@gmail.com>
2015-12-17 01:47:22 +0800

    add demo page to see the result

---
+++ demo.js
@@ -0,0 +1,11 @@
+goog.require('goog.dom')
+goog.require('wythe.lowpolybg')
+
+window.onload = function() {
+    var body = goog.dom.getElementsByTagNameAndClass('body')[0];
+    var canvas = wythe.lowpolybg.createLowPolyCanvas('HelloWorld', body, {});
+    canvas.style.left = '0px';
+    canvas.style.top = '0px';
+    canvas.style.position = 'absolute';
+    body.appendChild(canvas);
+}
